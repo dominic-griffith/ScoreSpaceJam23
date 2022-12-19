453f736ffffd2dae7593f8c743857149129c6869
dominic-griffith <dominicgriffith15@gmail.com>
2022-12-18 19:46:53 -0800

    Snow Shader

---
+++ Assets/Scripts/UI/Stopwatch.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class Stopwatch : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _currentTimeText;
+
+    private float _currentTime;
+    private bool _stopwatchActive;
+
+    private void Start()
+    {
+        _currentTime = 0f;
+        StartStopwatch();
+    }
+
+    private void Update()
+    {
+        IncrementTime();
+    }
+
+    private void IncrementTime()
+    {
+        if(_stopwatchActive == true)
+        {
+            _currentTime += Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(_currentTime);
+        _currentTimeText.text = time.ToString(@"mm\:ss\:fff");
+    }
+
+    public void StartStopwatch()
+    {
+        _stopwatchActive = true;
+    }
+
+    public void StopStopwatch()
+    {
+        _stopwatchActive = false;
+    }
+}
